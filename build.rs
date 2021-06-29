b731c5d344197fdd20c72e914874f86f688e734b
Ricky12Awesome <rickygraveen@gmail.com>
2021-06-29 04:58:51 -0500

    UAC will prompt when running

--- build.rs
+++ build.rs
@@ -1,11 +1,28 @@
 use std::io;
-#[cfg(windows)] use winres::WindowsResource;
 
 fn main() -> io::Result<()> {
   #[cfg(windows)] {
+    use winres::WindowsResource;
+
     WindowsResource::new()
       .set_icon("assets/icon.ico")
+      .set_language(winapi::um::winnt::MAKELANGID(
+        winapi::um::winnt::LANG_ENGLISH,
+        winapi::um::winnt::SUBLANG_ENGLISH_US,
+      ))
+      .set_manifest(r#"
+<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+<trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
+    <security>
+        <requestedPrivileges>
+            <requestedExecutionLevel level="requireAdministrator" uiAccess="false" />
+        </requestedPrivileges>
+    </security>
+</trustInfo>
+</assembly>
+"#)
       .compile()?;
   }
+
   Ok(())
 }
